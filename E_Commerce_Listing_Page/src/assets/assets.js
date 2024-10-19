1de908efc4b442d973f68db632cad0d57e3cbb83
Rupendra <136944198+RupendraCHA@users.noreply.github.com>
2024-10-19 13:33:36 +0530

    E Commerce Listing Page

---
+++ E_Commerce_Listing_Page/src/assets/assets.js
@@ -0,0 +1,56 @@
+// eslint-disable-next-line no-unused-vars
+import Nursary_1 from "./Nursary_1.png"
+import Nursary_2 from "./Nursary_2.png"
+import Nursary_3 from "./Nursary_3.png"
+import Nursary_4 from "./Nursary_4.png"
+import Nursary_5 from "./Nursary_5.png"
+import Nursary_6 from "./Nursary_6.png"
+import Nursary_7 from "./Nursary_7.png"
+import Nursary_8 from "./Nursary_8.png"
+import Nursary_9 from "./Nursary_9.png"
+import Nursary_10 from "./Nursary_10.png"
+
+export const nursary_list = [
+    {
+        menu_name: "Monstera Deliciosa – Large, holey leaves.",
+        menu_image: Nursary_10
+    },
+    {
+        menu_name: "Spider Plant – Air-purifying foliage plant.",
+        menu_image: Nursary_1
+    },
+    {
+        menu_name: "Snake Plant – Hardy, low-light tolerant.",
+        menu_image: Nursary_2
+    },
+    {
+        menu_name: "Peace Lily – Elegant, white flowering.",
+        menu_image: Nursary_3
+    },
+    {
+        menu_name: "Jade Plant – Succulent, thick leaves.",
+        menu_image: Nursary_4
+    },
+    {
+        menu_name: "Aloe Vera – Medicinal, spiky succulent.",
+        menu_image: Nursary_5
+    },
+    {
+        menu_name: "English Ivy – Trailing, lobed vine.",
+        menu_image: Nursary_6
+    },
+    {
+        menu_name: "Rubber Plant – Glossy, large leaves.",
+        menu_image: Nursary_7
+    },
+    {
+        menu_name: "Pothos Plant – Easy-care, heart-shaped.",
+        menu_image: Nursary_8
+    }
+    ,
+    {
+        menu_name: "ZZ Plant – Drought-tolerant, dark green.",
+        menu_image: Nursary_9
+    }
+    
+]
